1c43d3544f18c89664957c7f5245e9ba0d34794e
Restyled.io <commits@restyled.io>
2023-02-11 12:19:41 +0000

    Restyled by prettier

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,11 @@ function Model(props) {
   const { nodes, materials } = useGLTF("/gltf/shoe.gltf");
   return (
     <group {...props} dispose={null} scale={2.8}>
-      <mesh geometry={nodes.shoe.geometry} material={materials.laces} material-color={'red'} />
+      <mesh
+        geometry={nodes.shoe.geometry}
+        material={materials.laces}
+        material-color={"red"}
+      />
       <mesh geometry={nodes.shoe_1.geometry} material={materials.mesh} />
       <mesh geometry={nodes.shoe_2.geometry} material={materials.caps} />
       <mesh geometry={nodes.shoe_3.geometry} material={materials.inner} />
@@ -28,38 +32,53 @@ function App() {
           <div className="product-canvas">
             <Canvas>
               <Suspense fallback={null}>
-              <ambientLight />
-                  <spotLight 
-                   position={[10, 15, 10]}
-                   angle={0.1}
-                   penumbra={0.1}
-                   intensity={0.9}
-                   castShadow
-                  />
-                  <Model />
-                  <OrbitControls 
+                <ambientLight />
+                <spotLight
+                  position={[10, 15, 10]}
+                  angle={0.1}
+                  penumbra={0.1}
+                  intensity={0.9}
+                  castShadow
+                />
+                <Model />
+                <OrbitControls
                   enablePan={false}
                   enableZoom={false}
                   enableRotate={true}
-                  />
+                />
               </Suspense>
             </Canvas>
           </div>
           <h2>Select your color</h2>
           <div className="colors">
             <div>
-              <input type="color" id="head" name="head" defaultValue="#e66465" />
-              <label  htmlFor="head">Main</label>
+              <input
+                type="color"
+                id="head"
+                name="head"
+                defaultValue="#e66465"
+              />
+              <label htmlFor="head">Main</label>
             </div>
 
             <div>
-              <input type="color" id="body" name="body" defaultValue="#f6b73c" />
-              <label  htmlFor="body">Stripes</label>
+              <input
+                type="color"
+                id="body"
+                name="body"
+                defaultValue="#f6b73c"
+              />
+              <label htmlFor="body">Stripes</label>
             </div>
 
             <div>
-              <input type="color" id="body" name="body" defaultValue="#f6b73c" />
-              <label  htmlFor="body">Sole</label>
+              <input
+                type="color"
+                id="body"
+                name="body"
+                defaultValue="#f6b73c"
+              />
+              <label htmlFor="body">Sole</label>
             </div>
           </div>
         </div>
