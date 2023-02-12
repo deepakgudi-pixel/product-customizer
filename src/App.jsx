dd43e2c4913f928ef379bf7fa6fc82963e5aacf6
Restyled.io <commits@restyled.io>
2023-02-12 13:59:28 +0000

    Restyled by prettier

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,11 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import Model from "./components/Model";
 
-
 function App() {
-
-  const [mesh, setMesh] = useState('#ffffff');
-  const [stripes, setStripes] = useState('#ffffff');
-  const [sole, setSole] = useState('#ffffff');
-  const [laces, setLaces] = useState('#ffffff');
+  const [mesh, setMesh] = useState("#ffffff");
+  const [stripes, setStripes] = useState("#ffffff");
+  const [sole, setSole] = useState("#ffffff");
+  const [laces, setLaces] = useState("#ffffff");
 
   return (
     <div>
@@ -19,45 +17,75 @@ function App() {
           <div className="product-canvas">
             <Canvas>
               <Suspense fallback={null}>
-              <ambientLight />
-                  <spotLight 
-                   position={[10, 15, 10]}
-                   angle={0.1}
-                   penumbra={0.1}
-                   intensity={0.9}
-                   castShadow
-                  />
-                  <Model customColors={{ mesh: mesh, stripes: stripes, sole: sole, laces: laces }}/>
-                  <OrbitControls 
+                <ambientLight />
+                <spotLight
+                  position={[10, 15, 10]}
+                  angle={0.1}
+                  penumbra={0.1}
+                  intensity={0.9}
+                  castShadow
+                />
+                <Model
+                  customColors={{
+                    mesh: mesh,
+                    stripes: stripes,
+                    sole: sole,
+                    laces: laces,
+                  }}
+                />
+                <OrbitControls
                   enablePan={false}
                   enableZoom={false}
                   enableRotate={true}
-                  />
+                />
               </Suspense>
             </Canvas>
           </div>
           <h2>Select your color</h2>
           <div className="colors">
             <div>
-              <input type="color" id="mesh" name="mesh" defaultValue={mesh} onChange={(e)=> setMesh(e.target.value)} />
-              <label  htmlFor="mesh">Main</label>
+              <input
+                type="color"
+                id="mesh"
+                name="mesh"
+                defaultValue={mesh}
+                onChange={(e) => setMesh(e.target.value)}
+              />
+              <label htmlFor="mesh">Main</label>
             </div>
 
             <div>
-              <input type="color" id="stripes" name="stripes" defaultValue={stripes} onChange={(e)=> setStripes(e.target.value)}  />
-              <label  htmlFor="stripes">Stripes</label>
+              <input
+                type="color"
+                id="stripes"
+                name="stripes"
+                defaultValue={stripes}
+                onChange={(e) => setStripes(e.target.value)}
+              />
+              <label htmlFor="stripes">Stripes</label>
             </div>
 
             <div>
-              <input type="color" id="sole" name="sole" defaultValue={sole} onChange={(e)=> setSole(e.target.value)} />
-              <label  htmlFor="body">Sole</label>
+              <input
+                type="color"
+                id="sole"
+                name="sole"
+                defaultValue={sole}
+                onChange={(e) => setSole(e.target.value)}
+              />
+              <label htmlFor="body">Sole</label>
             </div>
 
             <div>
-              <input type="color" id="laces" name="laces" defaultValue={laces} onChange={(e)=> setLaces(e.target.value)} />
-              <label  htmlFor="laces">Laces</label>
+              <input
+                type="color"
+                id="laces"
+                name="laces"
+                defaultValue={laces}
+                onChange={(e) => setLaces(e.target.value)}
+              />
+              <label htmlFor="laces">Laces</label>
             </div>
-
           </div>
         </div>
       </div>
--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -5,12 +5,28 @@ export default function Model(props) {
   const { nodes, materials } = useGLTF("/gltf/shoe.gltf");
   return (
     <group {...props} dispose={null} scale={2.8}>
-      <mesh geometry={nodes.shoe.geometry} material={materials.laces}  material-color={props.customColors.laces} />
-      <mesh geometry={nodes.shoe_1.geometry} material={materials.mesh} material-color={props.customColors.mesh} />
+      <mesh
+        geometry={nodes.shoe.geometry}
+        material={materials.laces}
+        material-color={props.customColors.laces}
+      />
+      <mesh
+        geometry={nodes.shoe_1.geometry}
+        material={materials.mesh}
+        material-color={props.customColors.mesh}
+      />
       <mesh geometry={nodes.shoe_2.geometry} material={materials.caps} />
       <mesh geometry={nodes.shoe_3.geometry} material={materials.inner} />
-      <mesh geometry={nodes.shoe_4.geometry} material={materials.sole} material-color={props.customColors.sole} />
-      <mesh geometry={nodes.shoe_5.geometry} material={materials.stripes} material-color={props.customColors.stripes} />
+      <mesh
+        geometry={nodes.shoe_4.geometry}
+        material={materials.sole}
+        material-color={props.customColors.sole}
+      />
+      <mesh
+        geometry={nodes.shoe_5.geometry}
+        material={materials.stripes}
+        material-color={props.customColors.stripes}
+      />
       <mesh geometry={nodes.shoe_6.geometry} material={materials.band} />
       <mesh geometry={nodes.shoe_7.geometry} material={materials.patch} />
     </group>
